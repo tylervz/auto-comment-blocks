d7012dbf862d7266f79489d92280ed20c066e590
Tyler VanZanten <vanzanten.tyler@gmail.com>
2026-03-31 12:06:42 -0400

    Get the tests running

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
-  files: 'out/test/**/*.test.js',
+  files: 'out/src/test/**/*.test.js',
+  workspaceFolder: './',
+  launchArgs: [
+    '--user-data-dir=/tmp/vscode-test-data'
+  ]
 });
